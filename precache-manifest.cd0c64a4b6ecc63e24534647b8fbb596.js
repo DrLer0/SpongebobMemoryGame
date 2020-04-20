3bc1fa7de9165cfc81c0e7c12dfad9d13122b1a6
Jonathan Doctolero <jonathan.doctolero7@gmail.com>
2020-04-19 23:25:54 -0700

    Updates

---
+++ precache-manifest.cd0c64a4b6ecc63e24534647b8fbb596.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4644f30f6be55174d237adc4970d7a46",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "036e7b11b701bf63f5ba",
+    "url": "/MemoryGame/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f8f08788eee97baeee67",
+    "url": "/MemoryGame/static/js/2.ccb8d7f6.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/MemoryGame/static/js/2.ccb8d7f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "036e7b11b701bf63f5ba",
+    "url": "/MemoryGame/static/js/main.5fbaa8cf.chunk.js"
+  },
+  {
+    "revision": "815adc4d7d2a675603d5",
+    "url": "/MemoryGame/static/js/runtime-main.819cb7df.js"
+  },
+  {
+    "revision": "de84ca5439ea9fb0044414f99fba917f",
+    "url": "/MemoryGame/static/media/HandsomeSquidward.de84ca54.png"
+  },
+  {
+    "revision": "7d9c9565ab595eefd48ff3d52d217ff2",
+    "url": "/MemoryGame/static/media/Patrick.7d9c9565.png"
+  },
+  {
+    "revision": "ff5ca31b7e88a9b2abcce9d4bc14ce34",
+    "url": "/MemoryGame/static/media/SandyCheeks.ff5ca31b.jpg"
+  },
+  {
+    "revision": "095cb721679c86889ea4765e3404cdd7",
+    "url": "/MemoryGame/static/media/Suds.095cb721.png"
+  },
+  {
+    "revision": "aeb1b88685f1c487a0ebcca274246143",
+    "url": "/MemoryGame/static/media/drink.aeb1b886.png"
+  },
+  {
+    "revision": "ac92150d8393200ff2e522e51e1c9254",
+    "url": "/MemoryGame/static/media/gary.ac92150d.png"
+  },
+  {
+    "revision": "fa36b06caf47ca85eedf49d0a46936b0",
+    "url": "/MemoryGame/static/media/krabbypatty.fa36b06c.png"
+  },
+  {
+    "revision": "7be76b994f03af70a4dd9fad58a7c88e",
+    "url": "/MemoryGame/static/media/mrKrabs.7be76b99.png"
+  },
+  {
+    "revision": "cca740bed7561b09a0d6a1d12e3b67de",
+    "url": "/MemoryGame/static/media/pearl.cca740be.png"
+  },
+  {
+    "revision": "bc26e2c47c590c53957af7a3b1f5fe44",
+    "url": "/MemoryGame/static/media/planketon.bc26e2c4.png"
+  },
+  {
+    "revision": "1724279f6f9d5ab3926cad1a3a3df295",
+    "url": "/MemoryGame/static/media/sPonGEbOB.1724279f.png"
+  },
+  {
+    "revision": "ec3a7dc14984d7cca698494a6fd8f43c",
+    "url": "/MemoryGame/static/media/spongegar.ec3a7dc1.png"
+  }
+]);
